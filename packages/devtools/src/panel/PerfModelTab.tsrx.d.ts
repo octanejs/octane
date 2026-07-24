1ecdce8d7ce68928e95593f0221e3bb2d61259ba
Alem Tuzlak <t.zlak@hotmail.com>
2026-07-24 15:11:20 +0200

    feat(devtools): Performance-model tab (slowest mounts + queue hotspots)

---
+++ packages/devtools/src/panel/PerfModelTab.tsrx.d.ts
@@ -0,0 +1,6 @@
+// Declaration companion for PerfModelTab.tsrx.
+import type { OctaneDevtoolsEventClient } from '../client';
+
+export declare function PerfModelTab(props: { client: OctaneDevtoolsEventClient }): unknown;
+
+export {};
